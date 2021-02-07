1f911259a816e202272162bd7749a3e1076df49c
Kvazar-harubdu <volodimrmihnyk@gmail.com>
2021-02-07 13:19:22 +0200

    add catalog

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,67 @@
 $(function () {
 
+  $('.catalog__open').on('click', function () {
+    $('.catalog__filter').addClass('catalog__filter--active');
+  });
+
+  $('.catalog__close').on('click', function () {
+    $('.catalog__filter').removeClass('catalog__filter--active');
+  });
+
+  $('.product__small-slider').slick({
+    asNavFor: '.product__big-slider',
+    arrows: false,
+    focusOnSelect: true,
+    draggable: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+  });
+  $('.product__big-slider').slick({
+    asNavFor: '.product__small-slider',
+    arrows: false,
+    slidesToShow: 1,
+    draggable: false,
+    fade: true,
+  });
+
+  $('.catalog__filter-btn').on('click', function () {
+    $('.catalog__filter-btn').removeClass('catalog__filter-btn--active');
+    $(this).addClass('catalog__filter-btn--active');
+  });
+
+  $('.catalog__filter-btn--list').on('click', function () {
+    $('.product-card').addClass('product-card--list');
+  });
+
+  $('.catalog__filter-btn--grid').on('click', function () {
+    $('.product-card').removeClass('product-card--list');
+  });
+
+  $('.catalog__filter-btn--list').on('click', function () {
+    $('.pagination').addClass('pagination--size');
+  });
+
+  $('.catalog__filter-btn--grid').on('click', function () {
+    $('pagination').removeClass('pagination--size');
+  });
+
+  $('.catalog__filter-btn--list').on('click', function () {
+    $('.bread-crumbs__title,.bread-crumbs__link:contains("Product list 1")').html('Product list 2');
+  });
+  $('.catalog__filter-btn--grid').on('click', function () {
+    $('.bread-crumbs__title,.bread-crumbs__link:contains("Product list 2")').html('Product list 1');
+  });
+
+  $('.catalog__filter-btn--list').on('click', function () {
+    $('.product-card__price:contains("$29.00")').html('23$');
+  });
+  $('.catalog__filter-btn--grid').on('click', function () {
+    $('.product-card__price:contains("23$")').html('$29.00');
+  });
+
+  $('.catalog__selrct, .product__number').styler();
+
   $('.filter-products__stars').rateYo({
     starWidth: "11px",
     normalFill: "#d6d6d6",
@@ -8,11 +70,18 @@ $(function () {
     readOnly: true
   });
 
+  $('.product-card__stars').rateYo({
+    starWidth: "18px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    spacing: "12px",
+    readOnly: true
+  });
 
   $('.filter-prise__input').ionRangeSlider({
-    type: "double",
     prefix: "$",
-
+    prettify_enabled: true,
+    prettify_separator: '.',
     onStart: function (data) {
       $('.filter-prise__from').text(data.from);
       $('.filter-prise__to').text(data.to);
@@ -22,7 +91,6 @@ $(function () {
       $('.filter-prise__to').text(data.to);
     },
   });
-
 
   $('.burger-menu').on('click', function () {
     $('.menu__list').toggleClass('menu__list--active');
@@ -61,12 +129,12 @@ $(function () {
 
   var mixer1 = mixitup('.gallery__inner', {
     selectors: {
-      control: '.filter__btn'
+      control: '.mix-filter__btn'
     }
   });
   var mixer2 = mixitup('.design__inner', {
     selectors: {
-      control: '.filter__button'
+      control: '.mix-filter__button'
     }
   });
 
